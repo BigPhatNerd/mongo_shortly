38a38b7f8e241bfd871f297e616e00f9b744675e
BigPhatNerd <wilsonhorrell@gmail.com>
2021-09-09 09:25:41 -0500

    Forgot to include home route in server.

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ const app  = express();
 app.use(express.json({ extended: false }));   
 
 app.use(routes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, () =>{
 	console.log(`Server listening on ${PORT}`);
 });
